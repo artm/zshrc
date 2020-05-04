2af04fb58bf83b078e2559264cc10bf8db7b8647
Artem Baguinski <a.baguinski@performation.com>
2020-05-04 09:41:37 +0200

    Readd user@hostname to the prompt

--- prompt.zsh
+++ prompt.zsh
@@ -1,4 +1,4 @@
-typeset -aHg AGNOSTER_PROMPT_SEGMENTS=(prompt_status)
+typeset -aHg AGNOSTER_PROMPT_SEGMENTS=(prompt_status prompt_context)
 if [[ -f ~/.kube/config ]] ; then
   prompt_kube_ctx() {
     local ctx=$(awk '/current-context/{print $2;}' < ~/.kube/config)
